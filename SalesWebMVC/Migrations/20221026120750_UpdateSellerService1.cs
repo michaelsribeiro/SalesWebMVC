4907aea2b146e8a38dbe598c7813db479819328d
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-10-26 12:44:58 -0300

    aa

---
+++ SalesWebMVC/Migrations/20221026120750_UpdateSellerService1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMVC.Migrations
+{
+    public partial class UpdateSellerService1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Departments_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Seller",
+                newName: "DepartmentsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Departments_DepartmentsId",
+                table: "Seller",
+                column: "DepartmentsId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Departments_DepartmentsId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentsId",
+                table: "Seller",
+                newName: "DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentsId",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Departments_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ SalesWebMVC/Services/DepartmentsService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class DepartmentsService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public DepartmentsService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departments> FindAll()
+        {
+            return _context.Departments.OrderBy(x => x.Name).ToList();
+        }
+
+    }
+}
