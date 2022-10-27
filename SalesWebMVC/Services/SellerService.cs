365dbd402a11b654e382be28c035ae4812f6626a
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-10-26 22:37:40 -0300

    adjusts

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -32,7 +32,9 @@ namespace SalesWebMVC.Services
 
         public async Task<Seller> FindById(int id)
         {
-            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
+            return await _context.Seller
+                        .Include(obj => obj.Department)
+                        .FirstOrDefaultAsync(obj => obj.Id == id);
         }
 
         public async Task Remove(int id)
